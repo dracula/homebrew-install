c24472dd015548c10283bd117da6c7ef737192b6
Daniel Bayley <daniel.bayley@me.com>
2022-10-08 02:37:55 +0100

    Add BetterDiscord

---
+++ Casks/dracula-betterdiscord.rb
@@ -0,0 +1,21 @@
+cask "dracula-betterdiscord" do
+  version :latest
+  sha256 :no_check
+
+  app_name = "BetterDiscord"
+  tokens = token.split "-"
+  repo = "githubusercontent.com/#{tokens.first}/#{app_name}"
+  branch = "master"
+  css = "Dracula.theme.css"
+
+  url "https://raw.#{repo}/#{branch}/#{css}", verified: repo
+  name "Dracula for #{app_name}"
+  desc "Dark theme for #{app_name}"
+  homepage "https://#{tokens.first}theme.com/#{tokens.last}"
+
+  depends_on cask: "#{tokens.last}-installer"
+
+  themes = Pathname "#{Dir.home}/Library/Application Support/#{app_name}/themes"
+
+  artifact css, target: themes/css
+end
