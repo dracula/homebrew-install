5f14c4c4b5a1b852b7aedb23a537278cbc3e2648
Daniel Bayley <daniel.bayley@me.com>
2022-10-27 17:36:25 +0100

    Add Insomnia 1.0.2

---
+++ Casks/dracula-insomnia.rb
@@ -0,0 +1,24 @@
+cask "dracula-insomnia" do
+  version "1.0.2"
+  sha256 :no_check
+
+  tokens = token.split "-"
+  repo = File.join "github.com", tokens
+  branch = "master"
+  app_name = tokens.last.capitalize
+
+  url "https://#{repo}/archive/#{branch}.zip", verified: repo
+  name "Dracula for #{app_name}"
+  desc "Dark theme for #{app_name}"
+  homepage "https://#{tokens.first}theme.com/#{tokens.last}"
+
+  plugins = "#{Dir.home}/Library/Application Support/#{app_name}/plugins"
+  plugin = "#{tokens.last}-plugin-theme-#{tokens.first}"
+
+  livecheck do
+    url "https://#{tokens.last}.rest/plugins/#{plugin}"
+    regex /Version\s.*?(\d+(?:\.\d+)+)/
+  end
+
+  artifact "#{tokens.last}-#{branch}", target: "#{plugins}/#{plugin}"
+end
