b85cc82e7fb67ccace53f7a05e18b676e6b8d00f
Daniel Bayley <daniel.bayley@me.com>
2023-02-18 18:18:29 +0000

    Add Xcode

---
+++ Casks/dracula-xcode.rb
@@ -0,0 +1,24 @@
+cask "dracula-xcode" do
+  version :latest
+  sha256 :no_check
+
+  tokens = token.split "-"
+  repo = File.join "githubusercontent.com", tokens
+  branch = "master"
+  theme = "Dracula.xccolortheme"
+
+  app_name = tokens.last.capitalize
+  mas_id = "497799835"
+
+  url "https://raw.#{repo}/#{branch}/#{theme}", verified: repo
+  name "Dracula for #{app_name}"
+  desc "Dark theme for #{app_name}"
+  homepage "https://#{tokens.first}theme.com/#{tokens.last}"
+
+  artifact theme, target: "#{Dir.home}/Library/Developer/Xcode/UserData/FontAndColorThemes/#{theme}"
+
+  preflight do
+    error = "#{app_name} is not installed. Try: mas install #{mas_id}"
+    raise error unless (appdir/"#{app_name}.app").directory?
+  end
+end
