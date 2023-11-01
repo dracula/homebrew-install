6b5a5f840cc02a2c44546c8cd1d647596f117c03
Daniel Bayley <daniel.bayley@me.com>
2023-11-01 02:10:07 +0000

    Add wallpaper

---
+++ Casks/dracula-wallpaper.rb
@@ -0,0 +1,28 @@
+require "active_support/core_ext/string/inflections"
+
+cask "dracula-wallpaper" do
+  version :latest
+  sha256 :no_check
+
+  tokens = token.split "-"
+  repo = File.join "github.com", tokens
+  branch = "master"
+
+  url "https://#{repo}/archive/#{branch}.zip", verified: repo
+  name token.titlecase
+  desc "Wallpapers with the theme and colors of Dracula"
+  homepage "https://#{tokens.first}theme.com/#{tokens.last}"
+
+  wallpapers = "#{Dir.home}/Library/Desktop Pictures"
+
+  %w[base macos].each do |wallpaper|
+    artifact "#{tokens.last}-#{branch}/first-collection/#{wallpaper}.png",
+             target: "#{wallpapers}/Dracula-#{wallpaper}.png"
+  end
+
+  caveats <<~EOS
+    Be sure to add the #{wallpapers} folder:
+      System Settings > Wallpaper > Add Folder…
+      open x-apple.systempreferences:com.apple.Wallpaper-Settings.extension
+  EOS
+end
